8069753fd980ed12504b0281c3470c42a23d0a72
Sanjaya-Samudra <sanjayasamudra2003@gmail.com>
2025-11-09 22:03:40 +0530

    Implement responsive dashboard layout with sidebar functionality; add header component and adjust styles for various screen sizes

--- Hospital-Management-System-Front-End/Hospital-Management-Front-End/src/app/page/dash-board-page/dash-board-page.component.ts
+++ Hospital-Management-System-Front-End/Hospital-Management-Front-End/src/app/page/dash-board-page/dash-board-page.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavComponent } from "../../common/nav/nav.component";
+import { HeaderComponent } from "../../common/header/header.component";
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../../common/footer/footer.component';
 
 @Component({
   selector: 'app-dash-board-page',
   standalone: true,
-  imports: [NavComponent, FooterComponent ,RouterOutlet],
+  imports: [NavComponent, HeaderComponent, FooterComponent, RouterOutlet],
   templateUrl: './dash-board-page.component.html',
   styleUrl: './dash-board-page.component.css'
 })
 export class DashBoardPageComponent {
+  @ViewChild(NavComponent) navComponent!: NavComponent;
+  isSidebarCollapsed = false;
 
+  onSidebarCollapsed(collapsed: any) {
+    this.isSidebarCollapsed = collapsed as boolean;
+  }
 }
